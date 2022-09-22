ba1d75f06d19c2a55acc1ec3d86a428bc21ab57a
David ML <darkslayer_zerox8@hotmail.com>
2022-09-22 14:58:16 -0500

    Se añadio la funcionalidad de consultar datos de un Json mediante un ciclo utilizando HTTP [Class 10]

--- src/app/customers/customers.page.ts
+++ src/app/customers/customers.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; //Importar el router de angular
+import { HttpClient } from '@angular/common/http'; //Plugin de HTTP para consultar peticiones
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-customers',
@@ -8,13 +10,29 @@ import { Router } from '@angular/router'; //Importar el router de angular
 })
 export class CustomersPage implements OnInit {
 
-  constructor(private router: Router) { } //Implementamos el router en el constructor
+  customers: any = [];
+
+  constructor(private router: Router, private http: HttpClient) { } //Implementamos el router, http en el constructor
 
   ngOnInit() {
+    this.getCustomers().subscribe(res => {
+      console.log('Res:', res)
+      this.customers = res;
+    });
   }
 
   goToHome(){
     this.router.navigate(['/home']) //Generamos la funcion para el redireccionamiento
+  }
+
+  getCustomers(){
+    return this.http
+    .get('assets/files/customers.json')
+    .pipe(
+      map((res:any) => {
+        return res.data;
+      })
+    )
   }
 
 }
