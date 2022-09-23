0bfc456971d6cad582d1e1146232d3fde08c362d
David ML <darkslayer_zerox8@hotmail.com>
2022-09-23 10:33:07 -0500

    Se cambio el comportamiento de las alertar de la última clase [Fix #1]

--- src/app/cities/cities.page.ts
+++ src/app/cities/cities.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; //Importar el router de angular
 import { HttpClient } from '@angular/common/http'; //Plugin de HTTP para consultar peticiones
 import { map } from 'rxjs/operators';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cities',
@@ -12,7 +14,12 @@ export class CitiesPage implements OnInit {
 
   cities: any = [];
 
-  constructor(private router: Router, private http: HttpClient) { }
+  constructor(
+      private router: Router,
+      private http: HttpClient,
+      public toastController: ToastController,
+      public alertController: AlertController
+    ) { }
 
   ngOnInit() {
     this.getCties().subscribe(res => {
@@ -29,6 +36,54 @@ export class CitiesPage implements OnInit {
         return res.data;
       })
     )
+  }
+
+  async presentToast(message) { //Funcion para crear un toast alert
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom'
+    });
+
+    toast.present()
+  }
+
+  async presentAlert(message){ //funcion para crear una alerta de un solo boton
+    const alert = await this.alertController.create({
+      header: 'Eliminar ciudad',
+      message: message,
+      buttons: ['Aceptar']
+    });
+
+    await alert.present();
+    let result = await alert.onDidDismiss();
+    console.log(result);
+  }
+
+  async presentAlertOptions(){ //Funcion para crear una alerta con opciones que desencadenan otras acciones
+    const alert = await this.alertController.create({
+      header: 'Eliminar ciudad',
+      message: '¿Eliminar la ciudad?',
+      buttons:
+      [
+        {
+          text: 'No',
+          handler: () => {
+              this.presentToast('Ciudad no eliminada');
+            }
+        },
+        {
+          text: 'Si',
+          handler: () => {
+              this.presentToast('Ciudad eliminada');
+            }
+        }
+      ]
+    });
+
+    await alert.present();
+    let result = await alert.onDidDismiss();
+    console.log(result);
   }
 
 }
